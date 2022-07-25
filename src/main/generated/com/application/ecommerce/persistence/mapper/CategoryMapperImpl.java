a9e1dc0e078df0d4245e3e033b7138013b9da8d6
Gustavo Pino <gustavopino@hotmail.es>
2022-07-25 14:34:05 -0300

    add documentation swagger

--- src/main/generated/com/application/ecommerce/persistence/mapper/CategoryMapperImpl.java
+++ src/main/generated/com/application/ecommerce/persistence/mapper/CategoryMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-07-25T13:19:06-0300",
+    date = "2022-07-25T14:31:49-0300",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 11.0.16 (Oracle Corporation)"
 )
 @Component
